6cdcfcda161f8f88f4d1abab0c07119d10b5b1ce
comp6905 <comp6905project@gmail.com>
2015-11-16 09:25:44 -0330

    database file updated.

---
+++ register.sql
@@ -0,0 +1,6 @@
+create table registermentor(
+    mentor_id int(11) AUTO_INCREMENT,
+    mentor_name varchar(50) not null,
+    mentor_email varchar(100) not null),
+    PRIMARY KEY (mentor_id)
+    );
